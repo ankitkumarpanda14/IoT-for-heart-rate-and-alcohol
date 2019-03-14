a82a55bcd031b985d75411b58e1986046778cede
ankitkumarpanda14 <35422387+ankitkumarpanda14@users.noreply.github.com>
2019-03-14 19:57:52 +0530

    Update Rpi_code.py

--- Rpi_code.py
+++ Rpi_code.py
@@ -9,11 +9,11 @@ time.sleep(2)
 while True:
   s=ArduinoSerial.readline()
   print s
-  temp=url.urlopen("https://api.thingspeak.com/update?api_key=M99N3460DVOYRKS3&field1="+str(s))
+  temp=url.urlopen("https://api.thingspeak.com/update?api_key=6T00OH5K5ENKA9HX&field1="+str(s))
   time.sleep(15)
   t=ArduinoSerial1.readline()
   print t
-  temp=url.urlopen("https://api.thingspeak.com/update?api_key=VEAT5M4PTBWVBOYQ&field1="+str(t))
+  temp=url.urlopen("https://api.thingspeak.com/update?api_key=6T00OH5K5ENKA9HX&field2="+str(t))
   time.sleep(15)
 
 
